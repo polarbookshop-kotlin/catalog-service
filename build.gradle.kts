98f796d53360bafe643af00b5ae1305540909241
Ruibin Zhang <ruibin.zhang021@icloud.com>
2024-08-09 15:28:30 +0200

    feat(build.gradle.kts): add spring-cloud-starter-config and spring-boot-starter-actuator dependencies to enable configuration management and monitoring features feat(build.gradle.kts): add spring-retry dependency for handling retry logic in the application feat(build.gradle.kts): import spring-cloud-dependencies BOM to manage versions of Spring Cloud dependencies feat(application.yml): configure Spring Cloud Config Server properties for centralized configuration management feat(application.yml): configure Spring Cloud Config Server retry properties for robustness feat(application.yml): expose /refresh endpoint for actuator to trigger configuration refresh in the application

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     kotlin("jvm") version "1.9.24"
     kotlin("plugin.spring") version "1.9.24"
 }
+val springCloudVersion by extra("2023.0.3")
 
 group = "com.polarbookshop"
 version = "0.0.1-SNAPSHOT"
@@ -24,6 +25,8 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.cloud:spring-cloud-starter-config")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("io.projectreactor:reactor-test")
@@ -31,6 +34,9 @@ dependencies {
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     // https://mvnrepository.com/artifact/io.github.microutils/kotlin-logging-jvm
     implementation("io.github.oshai:kotlin-logging-jvm:5.1.4")
+    // https://mvnrepository.com/artifact/org.springframework.retry/spring-retry
+    implementation("org.springframework.retry:spring-retry:2.0.6")
+
 }
 
 kotlin {
@@ -52,4 +58,9 @@ configurations {
 
 tasks.named<JavaExec>("bootRun") {
     systemProperty("spring.profiles.active", "testdata")
+}
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion")
+    }
 }
