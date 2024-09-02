d1e597165ea1cb801f10200716ea3b58979fd20c
Ruibin Zhang <ruibin.zhang021@icloud.com>
2024-09-03 00:32:16 +0200

    chore(pushimage.sh): update builder image to dashaun/builder:tiny for better performance The builder image in the pushimage.sh script has been updated to dashaun/builder:tiny to improve performance and efficiency during the image building process. This change ensures that a more optimized builder image is used for building the application image.

--- pushimage.sh
+++ pushimage.sh
@@ -4,4 +4,4 @@
   -PregistryUrl=ghcr.io \
   -PregistryUsername=polarbookshop-kotlin \
   -PregistryToken="$GHCR_TOKEN" \
-  --builder ghcr.io/thomasvitale/java-builder-arm64
+  --builder dashaun/builder:tiny
